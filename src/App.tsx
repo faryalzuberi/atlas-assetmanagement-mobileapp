29d30a8a6f73ed49bd2fe31f237148630192a009
Sheheryar Nadeem <sheheryarnadeem@Sheheryars-MacBook-Pro.local>
2021-06-22 16:09:23 +0500

    NavigationContainer Error Solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
-    <>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={<></>} />
-        <Tab.Screen name="Settings" component={<></>} />
-      </Tab.Navigator>
-    </>
+    <></>
   );
 };
 export default App;
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import App from './App';
-import {NavigationContainer} from '@react-navigation/native';
 
 const MainApp = () => {
   return (
-    <NavigationContainer>
       <App />
-    </NavigationContainer>
   );
 };
 
